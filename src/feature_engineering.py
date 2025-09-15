4d70c735f4f82c43d209b107a9f0d98191f6bf61
mian-yahya-gul <kakakhel176@gmail.com>
2025-09-15 16:19:36 +0500

    pipeline ready with all the components

---
+++ src/feature_engineering.py
@@ -0,0 +1,108 @@
+import pandas as pd 
+import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+import logging
+import yaml
+
+#ensure the "logs directory exists"
+log_dir = 'logs'
+os.makedirs(log_dir,exist_ok=True)  
+
+#loggin Configuration
+logger = logging.getLogger('feature_engineering')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path = os.path.join(log_dir, 'feature_engineering.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+def load_data(file_path: str) ->pd.DataFrame:
+    """Load data from the csv file"""
+    try:
+        df = pd.read_csv(file_path)
+        df.fillna('', inplace=True)
+        logger.debug('data loaded and NaNs filled from %s', file_path)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error('failed to pasrse the csv file: %s', e)
+        raise
+    except Exception as e:
+        logger.error('unexpected error occured while loading the data: %s', e)
+        raise
+
+def apply_tfidf(train_data: pd.DataFrame, test_data: pd.DataFrame, max_features:int) -> tuple:
+    """apply tfidf to the data"""
+    try:
+        vectorizer = TfidfVectorizer(max_features=max_features)
+
+        X_train = train_data['text'].values
+        y_train = train_data['target'].values
+        X_test = test_data['text'].values
+        y_test = test_data['target'].values
+
+        X_train_bow = vectorizer.fit_transform(X_train)
+        X_test_bow = vectorizer.transform(X_test)
+
+        # fit_transform(X_train) → learn vocabulary + create TF-IDF matrix for training.
+
+        # transform(X_test) → use the same vocabulary → create TF-IDF matrix for testing.
+
+        train_df = pd.DataFrame(X_train_bow.toarray())
+        train_df['label'] = y_train 
+
+        test_df = pd.DataFrame(X_test_bow.toarray())
+        test_df['label'] = y_test 
+
+        logger.debug('TFIDF applied and dataframe transformed')
+        return train_df, test_df
+    except Exception as e:
+        logger.error('error during bag of words transformation %s', e)
+        raise
+
+def save_data(df: pd.DataFrame, file_path: str) -> None:
+    """save the data to a csv file"""
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        df.to_csv(file_path, index=False)
+        logger.debug('data saved to %s', file_path)
+    except Exception as e:
+        logger.error('Unexpected error occured while saving the data: %s',e)
+        raise
+
+def main():
+    try:
+        max_features = 50
+        train_data = load_data('./data/interim/train_processed.csv')
+        test_data = load_data('./data/interim/test_processed.csv')
+
+        train_df, test_df = apply_tfidf(train_data, test_data, max_features)
+
+        save_data(train_df, os.path.join("./data", "processed", "train_tfidf.csv"))
+        save_data(test_df, os.path.join("./data", "processed", "test_tfidf.csv"))
+    except Exception as e:
+        logger.error("failed to complete the feature engineering process %s", e)
+        print(f"error: {e}")
+
+if __name__ =='__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
---
+++ src/model_evaluation.py
@@ -0,0 +1,121 @@
+import os
+import numpy as np 
+import pandas as pd
+import pickle 
+import json
+from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
+import logging 
+
+# Ensure the "logs" directory exists
+log_dir = 'logs'
+os.makedirs(log_dir, exist_ok=True)
+
+# logging configuration
+logger = logging.getLogger('model_evaluation')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path = os.path.join(log_dir, 'model_evaluation.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+def load_model(file_path: str):
+    """load the trained model from a file"""
+    try:
+        with open(file_path, 'rb') as file:
+            model=pickle.load(file)
+        logger.debug('model loaded from %s',file_path)
+        return model
+    except FileNotFoundError:
+        logger.error('file not found %s',file_path)
+        raise
+    except Exception as e:
+        logger.error('unexpected error occured while loading the model: %s', e)
+        raise
+
+def load_data(file_path : str) -> pd.DataFrame:
+    """load data from csv file"""
+    try:
+        df = pd.read_csv(file_path)
+        logger.debug('data loaded form %s', file_path)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error('failed to parse the csv file %s', e)
+        raise
+    except Exception as e:
+        logger.error('unexpected error occured while loading the data %s',e)
+        raise
+
+
+def evaluate_model(clf, X_test:np.ndarray, y_test: np.ndarray) ->dict:
+    """evalute the model and return the evaluation matrics """
+    try:
+        y_pred = clf.predict(X_test)
+        y_pred_proba = clf.predict_proba(X_test)[:,1]
+
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        auc = roc_auc_score(y_test, y_pred_proba)
+
+        metrics_dict = {
+            'accuracy':accuracy,
+            'precision':precision,
+            'recall' : recall,
+            'auc' : auc
+        }
+
+        logger.debug('model evaluation matrix calculated')
+        return metrics_dict
+    except Exception as e:
+        logger.error('error during model evaluation %s',e)
+        raise
+
+def save_metrics(metrics:dict, file_path:str)-> None:
+    """save the evaluation metrics to a json file"""
+    try:
+        #ensure the directory exists
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        with open(file_path, 'w') as file:
+            json.dump(metrics, file, indent=4)
+        logger.debug('metrics saved to %s', file_path)
+    except Exception as e:
+        logger.error('error occured while saving the metrics %s', e)
+        raise
+
+
+def main():
+    try:
+        clf = load_model('./models/model.pkl')
+        test_data = load_data('./data/processed/test_tfidf.csv')
+
+        X_test = test_data.iloc[:, :-1].values
+        y_test = test_data.iloc[:, -1].values
+
+        metrics = evaluate_model(clf, X_test, y_test)
+
+        save_metrics(metrics,'reports/metrics.json')
+
+    except Exception as e:
+        logger.error('failed to complete the model evaluation process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
